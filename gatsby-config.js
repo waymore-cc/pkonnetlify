9671b0dbc44b4841ea2848d043df03732bdf169d
waymore-cc <pawel.kamyszek@waymore.cc>
2019-10-30 17:47:47 +0100

    Gatsby has just been setup to query a sample blog post at /blog/my-first-post :)

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,5 +17,13 @@ module.exports = {
         },
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `markdown-pages`,
+        path: `${__dirname}/src/markdown-pages`,
+      },
+    },
+    `gatsby-transformer-remark`,
   ],
 }
